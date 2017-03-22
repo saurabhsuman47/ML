9231b04b84238860df04009266d1ccad0604e6b1
saurabh <saurabhsuman4797@gmail.com>
2017-03-22 19:44:44 +0530

    modified main

--- Building Machine Learning Systems with Python/ch8 - Recommendation/Predicting Movie Ratings (Neighborhood Approach)/main.py
+++ Building Machine Learning Systems with Python/ch8 - Recommendation/Predicting Movie Ratings (Neighborhood Approach)/main.py
@@ -4,32 +4,28 @@ Created on Thu Mar 16 15:39:06 2017
 
 @author: saurabh
 """
-
-from load_data import load_data
 import numpy as np
-from scipy.spatial import distance
-from sklearn.metrics import r2_score
 import math
 import os
+from scipy.spatial import distance
+from sklearn.metrics import r2_score, mean_squared_error
+from load_data import load_data
 from normalize import normalizer
 
-data_path = os.path.join("..","ml-100k","u.data")
+data_path = os.path.join("ml-100k","u.data")
 train_reviews, test_reviews = load_data(data_path, test_percentage = 10)
 num_users, num_movies = train_reviews.shape
 
-binary = train_reviews > 0
-
+binary = train_reviews > 0 
 norm = normalizer()
 train_reviews = norm.fit_transform(train_reviews)
 
-
- 
 dist_between_users = distance.pdist(binary, metric = 'correlation') 
 dist_between_users = distance.squareform(dist_between_users)
 neighbors = dist_between_users.argsort(axis = 1)
 
 filled = train_reviews.copy()
-flag = 0
+
 for user in range(num_users):
     for movie in range(num_movies):
         if binary[user][movie] == True:
@@ -43,7 +39,7 @@ for user in range(num_users):
         neighbor_ratings = neighbor_ratings[:ratings_to_consider]
         filled[user][movie] = np.mean(neighbor_ratings)
 
-##when there is no movie for rating in training data (ratings went to testing data), we get nan values which we need to rate 3 
+##when there is no movie for rating in training data (ratings went to testing data), we get nan values which we need to rate average rating value 
 average_rating = (train_reviews.sum())/(binary.sum())           
 for user in range(num_users):
     for movie in range(num_movies):
@@ -53,8 +49,9 @@ for user in range(num_users):
 filled = norm.inverse_transform(filled)
 
 r2 = r2_score(test_reviews[test_reviews > 0.0], filled[test_reviews > 0.0])
-print("R2 Score - Binary neighbors (nearest half considered) : {}".format(r2))
+rmse =  np.sqrt(mean_squared_error(test_reviews[test_reviews > 0.0], filled[test_reviews > 0.0]))
+print("Binary matrix for dist calculation, with normalization (nearest half considered) - R2 score : {} , rmse : {}".format(r2, rmse))
 
-#R2 Score - Binary neighbors (all users considered) : 0.166722650833
-#R2 Score - Binary neighbors (nearest half considered) : 0.18
-#R2 Score - Normalized reviews for distance calculation (nearest half considered) : 0.295960386592
+#Binary matrix for dist calculation, no normalization (nearest half considered) - R2 score : 0.19078696037 , rmse : 1.01567341136
+#Binary matrix for dist calculation, with normalization (nearest half considered) - R2 score : 0.288603157857 , rmse : 0.949859444783
+#Normalized matrix for dist calculation (nearest half considered) - R2 score : 0.297425092775 , rmse : 0.940868649907
